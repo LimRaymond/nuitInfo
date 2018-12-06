4a668d27f8ba681e5fc23b92b5fcf055d574dace
ray <raymond.lim@epitech.eu>
2018-12-06 22:06:05 +0100

    Rectification error app.use

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/27017', {
+mongoose.connect('mongodb://localhost/test', {
     useMongoClient: true
 });
 
@@ -15,11 +15,10 @@ var db = mongoose.connection;
 
 //app.use(express.static(__dirname + '/../client'));
 
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 server.listen(8080);
--- server/todolist.js
+++ server/todolist.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/27017', {
+mongoose.connect('mongodb://localhost/test', {
     useMongoClient: true
 });
 

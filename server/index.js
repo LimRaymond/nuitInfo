c46366583631a4809bf114ab2a2fe1ab955d1bf4
Romain <romainlaemmel@gmail.com>
2018-12-06 21:04:16 +0100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+app.use(express.static(__dirname + '/../client'));
+
+server.listen(8080);

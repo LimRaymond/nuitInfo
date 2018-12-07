a337e5f1d463d93ee3ca69b4891a11f220633d29
sankamille <luc.brulet@epitech.eu>
2018-12-07 03:58:40 +0100

    [+] cors added

--- server/index.js
+++ server/index.js
@@ -3,18 +3,19 @@ const app = express();
 const server = require('http').createServer(app);
 const mongo = require('mongodb')
 const mongoose = require('mongoose')
+var cors = require('cors')
 const bodyParser = require('body-parser')
 const routes = require('./routes');
 
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost/test', {
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true }, function(err) {
+    if (err) throw err;
+    console.log("Connected");
 });
 
-var db = mongoose.connection;
+app.use(core());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes);
-server.listen(8080);
+server.listen(8081);
